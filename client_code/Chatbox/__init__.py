9b8d88a36efb3a5abd07b35e7ce8801b11ec235d
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-01-05 01:53:17 +0000

    Created Chatbox; Edited theme.css, settings and chatbox.html; Deleted Form2

--- client_code/Chatbox/__init__.py
+++ client_code/Chatbox/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import Form2Template
+from ._anvil_designer import ChatboxTemplate
 from anvil import *
 
-class Form2(Form2Template):
+class Chatbox(ChatboxTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
