a40850facd83dd1a466e41229a4a19044216b9ab
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-01-05 21:49:13 +0000

    Edited theme.css, settings and Chatbox.ChatTemplate

--- client_code/Chatbox/ChatTemplate/__init__.py
+++ client_code/Chatbox/ChatTemplate/__init__.py
@@ -4,6 +4,7 @@ from anvil import *
 class ChatTemplate(ChatTemplateTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
+        self.primary = app.theme_colors['Primary']
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
