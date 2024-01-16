73fb2a4b1e71913b148af6209120690167b26ade
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-01-16 16:23:20 +0000

    Edited TemplateForm1

--- client_code/TemplateForm1/__init__.py
+++ client_code/TemplateForm1/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import TemplateForm1Template
 from anvil import *
 from anvil_extras import routing
+from ..Form1 import Form1
 
 
 @routing.template(path="", priority=0, condition=None)
@@ -8,5 +9,5 @@ class TemplateForm1(TemplateForm1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
+        routing.set_url_hash('form1')
         # Any code you write here will run before the form opens.
