ef51a89bf73ce42c5baff368afd04ad792740bf4
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-01-16 17:09:33 +0000

    Edited startup2

--- client_code/startup2.py
+++ client_code/startup2.py
@@ -3,5 +3,22 @@ import anvil
 
 hash = anvil.get_url_hash()
 
-anvil.alert(hash)
+url_components = hash.split('?')
+
+if len(url_components) > 1:
+    pattern = url_components[0]
+    params = url_components[1]
+
+    query_dict = {}
+    for pair in params.split('&'):
+        key, value = pair.split('=')    
+        query_dict[key] = value
+else:
+    pattern = hash
+    query_dict = hash
+
+
+anvil.alert(pattern)
+# anvil.alert(query_dict)
+
 anvil.open_form('Chatbox')
