fad9a7037b1d78bf5f43ffb2b2905944a5d73149
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-01-17 03:57:22 +0000

    Edited Chatbox.ChatTemplate, Chatbox and chatbox2.html

--- client_code/Chatbox/ChatTemplate/__init__.py
+++ client_code/Chatbox/ChatTemplate/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import ChatTemplateTemplate
-from anvil import *
+import anvil
+
 
 class ChatTemplate(ChatTemplateTemplate):
     def __init__(self, **properties):
@@ -7,4 +8,29 @@ class ChatTemplate(ChatTemplateTemplate):
         self.primary = app.theme_colors['Primary']
         self.init_components(**properties)
 
-        # Any code you write here will run before the form opens.
+        if 'img' in self.item and self.item['img']:
+            self.link_loading.visible = True
+            self.link_loading.add_component(
+                anvil.Image(
+                    source='_/theme/loading.gif',
+                    role='load-message',
+                    width=28,
+                    height=14
+                ),
+                slot='img'
+            )
+            
+        if self.item['from'] == 'bot' and 'sources' in self.item:
+            self.fp_sources.visible = True
+            self.src_labels = []
+            for i in range(len(self.item['sources'])):
+                self.src_labels.append(
+                    anvil.Link(
+                        text=str(i + 1) + '. ' + self.item['sources'][i]['title'],
+                        url=self.item['sources'][i]['url'],
+                        foreground='black',
+                        role='src-link',
+                        border='solid 1px black'
+                    )
+                )
+                self.fp_source_links.add_component(self.src_labels[i])
