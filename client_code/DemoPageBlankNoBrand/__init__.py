f8946e3e59bd567b4cd37e28b46fa50d2ef20af3
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-01-25 22:57:28 +0000

    got rid of default message history

--- client_code/DemoPageBlankNoBrand/__init__.py
+++ client_code/DemoPageBlankNoBrand/__init__.py
@@ -1,8 +1,28 @@
 from ._anvil_designer import DemoPageBlankNoBrandTemplate
 from anvil import *
 
+msg_hist = [
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."},
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."},
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."},
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."},
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."}
+]
+
+
 class DemoPageBlankNoBrand(DemoPageBlankNoBrandTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
+        self.msg_hist = msg_hist
         self.init_components(**properties)
         # This has some theme color stuff in the background on the scroll bounce.
--- client_code/DemoPageHTMLMD3/__init__.py
+++ client_code/DemoPageHTMLMD3/__init__.py
@@ -1,9 +1,30 @@
 from ._anvil_designer import DemoPageHTMLMD3Template
 from anvil import *
 
+
+msg_hist = [
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."},
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."},
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."},
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."},
+            {'from': 'bot', 'text': 'Hi, how can I help you?'},
+            {'from': 'user', 'text': 'How do I do this thing?'},
+            {'from': 'bot', 'text': "Well that's easy. Just push the button."}
+]
+
+
 class DemoPageHTMLMD3(DemoPageHTMLMD3Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
+        self.msg_hist = msg_hist
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
