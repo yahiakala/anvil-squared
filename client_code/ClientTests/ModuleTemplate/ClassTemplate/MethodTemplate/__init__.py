e950dc8a414ab6c574eab5e0f0d158065cf4b138
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-02-06 16:18:51 +0000

    Edited ClientTests.ModuleTemplate.ClassTemplate.MethodTemplate and ClientTests

--- client_code/ClientTests/ModuleTemplate/ClassTemplate/MethodTemplate/__init__.py
+++ client_code/ClientTests/ModuleTemplate/ClassTemplate/MethodTemplate/__init__.py
@@ -22,7 +22,7 @@ class MethodTemplate(MethodTemplateTemplate):
             test_desc=self.item["ref"].__doc__,
             icon_size=self.item["icon_size"],
             item = self.item,
-            btn_run_function=self.btn_run_test_click
+            btn_run_function=self.run_test
         )
         self.add_component(self.test_method)
         self.add_event_handler('x-run', self.run_try_except)
@@ -44,3 +44,11 @@ class MethodTemplate(MethodTemplateTemplate):
         except Exception:
             self.success = False
             self.test_method.pass_fail_icon_change(self.success)
+
+    def run_test(self, **event_args):
+        """Run tests."""
+        if self.item['debug']:
+            self.btn_run_test_click()
+        else:
+            self.run_try_except()
+            
