722c09d69f48f4aa463c73d5e1db74d41e3fd31c
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-02-22 20:25:26 +0000

    Edited lemons

--- server_code/lemons.py
+++ server_code/lemons.py
@@ -7,15 +7,19 @@ import json
 url = "https://api.lemonsqueezy.com/v1/checkouts"
 
 
-def create_checkout(api_key=None, variants=[], store_id=None, selected_variant=None):
+def create_checkout(api_key=None, variants=[], store_id=None, selected_variant=None, user_email=None, test_mode=True):
     if not api_key:
         api_key = anvil.secrets.get_secret('lemon_api_key')
     if not variants:
-        variants = [261989, 262003, 262005]
+        variants = ['261989', '262003', '262005']
     if not store_id:
-        store_id = 71134
+        store_id = '71134'
     if not selected_variant:
-        selected_variant = 261989
+        selected_variant = '261989'
+    if not user_email:
+        user_email = 'name@example.com'
+    if not redirect_url:
+        redirect_url = 'https://dreambyte.ai'
     headers = {
         'Accept': 'application/vnd.api+json',
         'Content-Type': 'application/vnd.api+json',
@@ -28,43 +32,46 @@ def create_checkout(api_key=None, variants=[], store_id=None, selected_variant=N
             "attributes": {
                 # "custom_price": 50000,
                 "product_options": {
-                    "enabled_variants": variants
+                    "enabled_variants": variants,
+                    "redirect_url": redirect_url
                 },
                 "checkout_options": {
                     "button_color": "#2DD272"
                 },
                 "checkout_data": {
                     # "discount_code": "10PERCENTOFF",
+                    "email": user_email
                 },
-                "expires_at": "2024-10-30T15:20:06Z",
-                # "expires_at": None,
+                # "expires_at": "2024-10-30T15:20:06Z",
+                "expires_at": None,
                 "preview": True,
-                "test_mode": True
+                "test_mode": test_mode
             },
             "relationships": {
                 "store": {
                     "data": {
                         "type": "stores",
-                        "id": store_id
+                        "id": str(store_id)
                     }
                 },
                 "variant": {
                     "data": {
                         "type": "variants",
-                        "id": selected_variant
+                        "id": str(selected_variant)
                     }
                 }
             }
         }
     }
     json_data = json.dumps(data)
-    response = anvil.http.request(url=url, method="POST", data=json_data, headers=headers)
+    # response = anvil.http.request(url=url, method="POST", data=json_data, headers=headers)
 
-    # try:
-    #     response = anvil.http.request(url=url, method="POST", data=data, json=True, headers=headers)
-    # except anvil.http.HttpError as e:
-    #     print(e)
-    #     print(f"Error {e.status}")
+    try:
+        response = anvil.http.request(url=url, method="POST", data=data, json=True, headers=headers)
+    except anvil.http.HttpError as e:
+        print(e.content)
+        print(f"Error {e.status}")
 
     # print(response)
-    # print(response.get('content'))
+    # print(response['links']['self'])
+    print(response['data']['attributes']['url'])
