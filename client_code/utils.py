f359d34ab412cc06b7f8a5099e505593b2a7a984
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-03-14 15:26:12 +0000

    Edited utils

--- client_code/utils.py
+++ client_code/utils.py
@@ -6,8 +6,9 @@ def print_timestamp(input_str):
     print(input_str, " : ", dt.datetime.now().strftime("%H:%M:%S.%f"))
 
 
-def signup_with_email(tb_email, tb_password, tb_password_repeat, app_name, callable_name, lbl_error=None):
-    """Custom signup flow."""
+def signup_with_email(tb_email, tb_password, tb_password_repeat, app_name, callable_name, lbl_error):
+    """Signup with email with a custom server function and email confirmation."""
+    lbl_error.visible = False
     proceed = signup_with_email_checker(tb_email.text, tb_password.text, tb_password_repeat.text, lbl_error)
     user = None
     if proceed:
@@ -34,7 +35,7 @@ def signup_with_email(tb_email, tb_password, tb_password_repeat, app_name, calla
     return user
 
 
-def signup_with_email_checker(email, password, password_repeat, lbl_error=None):
+def signup_with_email_checker(email, password, password_repeat, lbl_error):
     """This method is called when the TextBox loses focus."""
     if len(email) < 5 or "@" not in email or "." not in email:
         lbl_error.text = "Enter an email address"
@@ -48,3 +49,33 @@ def signup_with_email_checker(email, password, password_repeat, lbl_error=None):
 
     lbl_error.visible = True
     return False
+
+
+def signin_with_email(tb_email, tb_password, callable_name, lbl_error):
+    """Signin with email with a custom server function."""
+    lbl_error.visible = False
+    user = None
+    try:
+        user = anvil.server.call(callable_name, tb_email.text, tb_password.text)
+    except anvil.users.MFARequired as e:
+        r = anvil.users.mfa.mfa_login_with_form(tb_email.text, tb_password.text)
+        if r == 'reset_mfa':
+            anvil.users.mfa.send_mfa_reset_email(tb_email.text)
+            lbl_error.text = "Requested 2-factor authentication reset for " + tb_email.text + ". Check your email."
+            lbl_error.visible = True
+        elif r == None:
+            lbl_error.text = "Cancelled login."
+            lbl_error.visible = True
+        else:
+            user = anvil.users.login_with_email(tb_email.text, tb_password.text, mfa=r, remember=True)
+    except anvil.users.AuthenticationFailed as e:
+        lbl_error.text = e.args[0]
+        lbl_error.visible = True
+    except anvil.users.EmailNotConfirmed as e:
+        lbl_error.text = "You haven't confirmed your email address. Please check your email and click the confirmation link, or reset your password."
+        lbl_error.visible = True
+    except anvil.users.TooManyPasswordFailures as e:
+        lbl_error.text = e.args[0]
+        lbl_error.visible = True
+
+    return user
