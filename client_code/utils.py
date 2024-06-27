ee73833587a5bdbd4830d97537ef21944188e540
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-06-27 22:21:17 +0000

    Created globals; Edited utils

--- client_code/utils.py
+++ client_code/utils.py
@@ -1,7 +1,6 @@
 import anvil.server
 import anvil.users
 import datetime as dt
-import anvil.js
 
 
 def print_timestamp(input_str):
@@ -97,45 +96,3 @@ def reset_password(tb_email, lbl_error):
             lbl_error.text = 'No user with this matching email was found.'
             lbl_error.visible = True
 
-
-class GlobalCache:
-    def __init__(self, global_dict):
-        self._global_dict = global_dict
-
-    def __getattr__(self, name):
-        if name in self._global_dict:
-            if name == 'user':
-                self._global_dict[name] = self._global_dict[name] or anvil.users.get_user()
-                print_timestamp('Global: user')
-            elif name == 'is_mobile':
-                self._global_dict[name] = self._global_dict[name] or anvil.js.window.navigator.userAgent.lower().find("mobi") > -1
-                print_timestamp('Global: is_mobile')
-            else:
-                print_timestamp(f'Global before: {name}')
-                if self._global_dict[name] is None:  # Only check None condition.
-                    self._global_dict[name] = anvil.server.call('get_' + name)
-                print_timestamp(f'Global after: {name}')
-            return self._global_dict[name]
-        raise AttributeError(f"Attribute {name} not found")
-
-    def __setattr__(self, name, value):
-        if name.startswith('_'):
-            # This allows initialization and internal attributes to be set.
-            super().__setattr__(name, value)
-        else:
-            if value is None:
-                # If setting an attribute to None, remove it to force repopulation on next access.
-                self._global_dict.pop(name, None)
-            else:
-                # For all other assignments, update the global dictionary normally.
-                self._global_dict[name] = value
-
-    def clear_global_attributes(self):
-        for name in list(self._global_dict.keys()):
-            self._global_dict[name] = None
-
-    def get_no_call(self, name):
-        """Get a global value but don't call the server if None."""
-        if name in self._global_dict:
-            return self._global_dict[name]
-        raise AttributeError(f"Attribute {name} not found")
