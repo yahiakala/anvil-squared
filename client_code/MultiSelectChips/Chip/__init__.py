9cf55c96864364bcb2d790195dc75943581523af
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-03 21:18:36 +0000

    Created MultiSelectChips.Chip, MultiSelectChips and MultiSelectChips.FilterChips; Edited settings

---
+++ client_code/MultiSelectChips/Chip/__init__.py
@@ -0,0 +1,14 @@
+from ._anvil_designer import ChipTemplate
+from anvil import *
+
+
+class Chip(ChipTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
+
+    def btn_chip_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.parent.raise_event("x-remove", remove=self.item['name'])
