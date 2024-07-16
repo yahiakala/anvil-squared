856fbff5bfea80db6a60c7e26b37fe0fd02ae613
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-16 20:53:04 +0000

    Edited helpers

--- server_code/helpers.py
+++ server_code/helpers.py
@@ -1,11 +1,12 @@
 import anvil.server
 from anvil.tables import app_tables
 from functools import wraps, partial
+import anvil.tables.query as q
 
 
 def run_callable():
     """
-    Make sure a special secret is defined when calling any callable from Anvil Squared.
+    Make sure a special secret is defined when calling any callable from the Anvil Squared dependency.
 
     This avoids any unintended server calls of callables in this app
     when using this app as a dependency.
@@ -15,12 +16,6 @@ def run_callable():
             _ = anvil.secrets.get_secret('SQUARED')
         except anvil.secrets.SecretError as e:
             print_timestamp(str(e.args[0]))
-
-
-@anvil.server.callable
-def ping():
-    print_timestamp("ping")
-    return "pong"
 
 
 def print_timestamp(input_str, verbose=True):
@@ -138,140 +133,6 @@ def proceed_or_wait(row, taskid, func_name=None, extra_keys=None):
     return row
 
 
-def permission_required(permissions):
-    """
-    Creates a decorator for callable functions.
-
-    Not seen by autocomplete.
-    """
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-        return anvil.server.callable(require_user=partial(check_user_auth, permissions=permissions))(wrapper)
-    return decorator
-
-
-def check_user_auth(user, permissions):
-    """Checks user auth from the auth columns in the user row."""
-    # TODO: replace false returns with raising value errors
-    print_timestamp('Checking user auth: ', permissions)
-    if user is None:
-        return False
-        
-    if isinstance(permissions, str):
-        required_permissions = set([permissions])
-    else:
-        required_permissions = set(permissions)
-
-    try:
-        user_permissions = set(
-            [
-                permission
-                for permission in required_permissions
-                if user[permission] == True
-            ]
-        )
-    except Exception:
-        return False
-
-    # ALL permissions option
-    # if not required_permissions.issubset(user_permissions):
-    #     return False
-
-    # ANY permissions option
-    if not len(user_permissions) > 0:
-        return False
-        
-    return True
-
-
-def user_table_auth(permissions):
-    """
-    Decorator that ensures the callable is seen by autocomplete.
-    Checks for boolean columns in the Users table with the permission names.
-    
-    Use this as a decorator below @anvil.server.callable
-    """
-    def auth_required_decorator(func):
-        @wraps(func)
-        def wrapped(*args, **kwargs):
-            user = anvil.users.get_user()
-            if not user:
-                raise ValueError("No user logged in.")
-            
-            #  Handle the fact that permissions might be a string or a list
-            if isinstance(permissions, str):
-                required_permissions = set([permissions])
-            else:
-                required_permissions = set(permissions)
-            
-            try:
-                user_permissions = set(
-                    [
-                        permission
-                        for permission in required_permissions
-                        if user[permission] == True
-                    ]
-                )
-            except Exception:
-                raise ValueError("Permission does not exist.")
-                
-            if len(user_permissions) == 0:
-                raise ValueError("Permission required.") 
-            else:
-                return func(*args, **kwargs)
-        return wrapped
-    return auth_required_decorator
-
-
-def sep_table_auth(permissions):
-    """
-    Checks user auth in separate 'Permissions' table.
-    
-    Use this as a decorator below @anvil.server.callable
-    """
-    pass
-
-
-def user_simple_auth(permissions):
-    """
-    Checks user auth in a simple object column called 'permissions'
-    
-    Use this as a decorator below @anvil.server.callable
-    """
-    def auth_required_decorator(func):
-        @wraps(func)
-        def wrapped(*args, **kwargs):
-            user = anvil.users.get_user()
-            if not user:
-                raise ValueError("No user logged in.")
-            
-            #  Handle the fact that permissions might be a string or a list
-            if isinstance(permissions, str):
-                required_permissions = set([permissions])
-            else:
-                required_permissions = set(permissions)
-            
-            try:
-                user_permissions = set(
-                    [
-                        permission
-                        for permission in required_permissions
-                        if permission in user['permissions'].keys()
-                    ]
-                )
-            except Exception:
-                raise ValueError("Permission required.")
-                
-            if len(user_permissions) == 0:
-                raise ValueError("Permission required.") 
-            else:
-                return func(*args, **kwargs)
-        return wrapped
-    return auth_required_decorator
-
-
 def user_bk_running(table_name, bk_name):
     """Check if a particular background task is running on this user."""
     user = anvil.users.get_user(allow_remembered=True)
@@ -291,3 +152,128 @@ def user_bk_running(table_name, bk_name):
             except anvil.server.BackgroundTaskNotFound:
                 pass
     return False
+
+
+# ------------------
+# Multi tenant stuff
+# ------------------
+def verify_tenant(tenant_id, user, tenant=None, usertenant=None):
+    """Verify a user is in this tenant."""
+    tenant = tenant or app_tables.tenants.get_by_id(tenant_id)
+    usertenant = usertenant or app_tables.usertenant.get(user=user, tenant=tenant)
+
+    if usertenant['tenant'] == tenant:
+        return tenant
+
+    raise Exception('User does not belong to this tenant.')
+
+
+def get_usertenant(tenant_id, user, tenant=None):
+    """Get a usermap. A user with no tenant will be added to this tenant."""
+    tenant = tenant or app_tables.tenants.get_by_id(tenant_id)
+    
+    if not app_tables.usertenant.get(user=user, tenant=tenant):
+        new_roles = get_new_user_roles(None, tenant)
+        usertenant = app_tables.usertenant.add_row(user=user, tenant=tenant, roles=new_roles)
+    else:
+        usertenant = app_tables.usertenant.get(user=user, tenant=tenant)
+    return usertenant
+
+
+def get_new_user_roles(tenant_id, tenant=None):
+    """Assign a brand new user a role in this tenant."""
+    tenant = tenant or app_tables.tenants.get_by_id(tenant_id)
+    new_roles = app_tables.roles.search(
+        tenant=tenant,
+        name=q.any_of(*tenant['new_roles']),
+        can_edit=q.not_(True)
+    )
+    return list(new_roles)
+
+
+def get_user_roles(tenant_id, user, usertenant=None, tenant=None):
+    """Get names of roles for a user in a tenant."""
+    tenant = tenant or verify_tenant(tenant_id, user, usertenant=usertenant)
+    usertenant = usertenant or app_tables.usertenant.get(user=user, tenant=tenant)
+
+    roles = []
+    if usertenant['roles']:
+        for role in usertenant['roles']:
+            roles.append(role['name'])
+    return list(set(roles))
+
+
+def get_permissions(tenant_id, user, tenant=None, usertenant=None):
+    """Get the permissions of a user in a particular tenant."""
+    tenant = tenant or verify_tenant(tenant_id, user, usertenant=usertenant)
+    usertenant = usertenant or app_tables.usertenant.get(user=user, tenant=tenant)
+        
+    user_permissions = []
+    if usertenant['roles']:
+        for role in usertenant['roles']:
+            if role['permissions']:
+                for permission in role['permissions']:
+                    user_permissions.append(permission['name'])
+
+    return list(set(user_permissions))
+
+
+def validate_user(tenant_id, user, usertenant=None, permissions=None, tenant=None):
+    usertenant = usertenant or get_usertenant(tenant_id, user, tenant=tenant)
+    tenant = tenant or verify_tenant(tenant_id, user, usertenant=usertenant)
+    permissions = permissions or get_permissions(tenant_id, user, usertenant=usertenant, tenant=tenant)
+    return tenant, usertenant, permissions
+
+
+def upsert_role(usertenant, role_name):
+    role = app_tables.roles.get(tenant=usertenant['tenant'], name=role_name)
+    if not usertenant['roles']:
+        usertenant['roles'] = [role]
+    elif role not in usertenant['roles']:
+        usertenant['roles'] = usertenant['roles'] + [role]
+    return usertenant
+
+
+def populate_permissions(role_dict, perm_list=None):
+    """Populate the permissions table."""
+    print_timestamp('populate_permissions')
+
+    if not perm_list:
+        perm_list = []
+        for key, val in role_dict.items():
+            perm_list = perm_list + val
+        perm_list = list(set(perm_list))
+    
+    for perm in perm_list:
+        if not app_tables.permissions.get(name=perm):
+            app_tables.permissions.add_row(name=perm)
+
+
+def populate_roles(tenant, role_dict):
+    """Some basic roles."""
+    print_timestamp('populate_roles')
+    
+    for key, val in role_dict.items():
+        perm_rows = app_tables.permissions.search(name=q.any_of(*val))
+        if len(perm_rows) == 0:
+            populate_permissions(role_dict)
+            perm_rows = app_tables.permissions.search(name=q.any_of(*val))
+            
+        is_it_there = app_tables.roles.get(name=key, tenant=tenant)
+        if not is_it_there:
+            app_tables.roles.add_row(name=key, tenant=tenant, permissions=list(perm_rows), can_edit=False)
+    return app_tables.roles.search(tenant=tenant)
+
+
+def create_tenant_single_squared(user, admin_role_name, role_dict):
+    """Create a tenant."""
+    if len(app_tables.tenants.search()) != 0:
+        raise anvil.server.InternalError('Only one tenant can exist in this instance.')
+
+    tenant = app_tables.tenants.add_row()
+    _ = populate_roles(tenant, role_dict)
+    admin_role = app_tables.roles.get(tenant=tenant, name=admin_role_name)
+    
+    _ = app_tables.usertenant.add_row(tenant=tenant, user=user, roles=[admin_role])
+    
+    return tenant
