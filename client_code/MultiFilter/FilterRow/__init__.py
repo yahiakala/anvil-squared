f5661f4790b723f8c72f8783800c0b300fc492e7
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-19 20:42:20 +0000

    Edited MultiFilter.FilterRow

--- client_code/MultiFilter/FilterRow/__init__.py
+++ client_code/MultiFilter/FilterRow/__init__.py
@@ -6,7 +6,15 @@ class FilterRow(FilterRowTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
         self.lbl_name.text = self.item['name']
         self.msc2.items = self.item['items']
         self.msc2.selected = self.item['selected']
+        self.selected = self.item['selected']
+        self.add_event_handler('x-clear-filter', self.clear_filter)
+    
+    def msc2_change(self, **event_args):
+        self.selected = self.msc2.selected
+
+    def clear_filter(self, **event_args):
+        self.msc2.selected = []
+        self.selected = []
