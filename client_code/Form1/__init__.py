4383300dfa56cb9e580e9039f8378c58cd2f04e3
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-20 04:29:30 +0000

    Created DemoMSTLayout and Form1; Edited MSTLayout

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,8 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+
+
+class Form1(Form1Template):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
