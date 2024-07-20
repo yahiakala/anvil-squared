2788d93c1321c2896af4efb0ed24d488929c2448
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-20 04:46:34 +0000

    Created MultiSelectTable.RowTemplate2; Edited DemoMST.RowTemplate1 and MultiSelectTable

--- client_code/DemoMST/RowTemplate1/__init__.py
+++ client_code/DemoMST/RowTemplate1/__init__.py
@@ -4,8 +4,9 @@ from anvil import *
 
 class RowTemplate1(RowTemplate1Template):
     def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
         self.init_components(**properties)
+
+    def update_button(self, **event_args):
         if self.item['selected']:
             self.btn_select.role = 'secondary-button-selected'
             self.btn_select.text = 'Selected'
@@ -17,5 +18,8 @@ class RowTemplate1(RowTemplate1Template):
         """This method is called when the button is clicked"""
         if self.item['selected']:
             self.parent.raise_event('x-remove-item', item=self.item)
+            self.item['selected'] = False
         else:
             self.parent.raise_event("x-add-item", item=self.item)
+            self.item['selected'] = True
+        self.update_button()
