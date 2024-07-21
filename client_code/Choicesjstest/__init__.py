720ec29f6a09dd80be03e4a0567818430fd45009
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-21 22:59:27 +0000

    Edited settings and Choicesjstest

--- client_code/Choicesjstest/__init__.py
+++ client_code/Choicesjstest/__init__.py
@@ -1,7 +1,13 @@
 from ._anvil_designer import ChoicesjstestTemplate
 from anvil import *
-from anvil.js.window import Choices
-import anvil.js
+# import anvil.js
+
+# from ..utils2._component_helpers import _html_injector
+
+# _html_injector.cdn("https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css")
+# _html_injector.cdn("https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js")
+
+
 
 class Choicesjstest(ChoicesjstestTemplate):
     def __init__(self, **properties):
@@ -10,6 +16,17 @@ class Choicesjstest(ChoicesjstestTemplate):
 
         # choices = anvil.js.window.Choices
         # dropdown_element = anvil.js.get_dom_node(self.dd)
+        # dropdown_element = self.dom_nodes['my-dropdown']
+        # self.choices = Choices(dropdown_element, {
+        #     'choices': [{'value': '1', 'label': 'Choice 1'},
+        #                 {'value': '2', 'label': 'Choice 2'},
+        #                 {'value': '3', 'label': 'Choice 3'}],
+        #     'removeItemButton': True
+        # })
+        
+    def form_show(self, **event_args):
+        """This method is called when the form is shown on the page"""
+        from anvil.js.window import Choices
         dropdown_element = self.dom_nodes['my-dropdown']
         self.choices = Choices(dropdown_element, {
             'choices': [{'value': '1', 'label': 'Choice 1'},
@@ -17,9 +34,6 @@ class Choicesjstest(ChoicesjstestTemplate):
                         {'value': '3', 'label': 'Choice 3'}],
             'removeItemButton': True
         })
-        
-    def form_show(self, **event_args):
-        """This method is called when the form is shown on the page"""
         self.call_js('setupChangeListener', self.choices, 'handle_change')
 
     def get_selected_items(self):
