393cc672158a0d58fa664f6ca7068e368ecbca19
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-07-22 20:17:45 +0000

    Edited DemoMSDD2

--- client_code/DemoMSDD2/__init__.py
+++ client_code/DemoMSDD2/__init__.py
@@ -10,3 +10,4 @@ class DemoMSDD2(DemoMSDD2Template):
         self.msdd2_1.items = ['option 1', 'option 2', 'option 3']
         self.msdd2_2.items = ['option 1', 'option 2', 'option 3']
         self.msdd2_3.items = ['option 1', 'option 2', 'option 3']
+        self.msdd2_4.items = ['option 1', 'option 2', 'option 3']
